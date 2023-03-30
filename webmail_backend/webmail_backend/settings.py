3d694cce37e211b94eaee13b4b3900270610deda
Adarsh Kumar <adarshku123654@gmail.com>
2023-03-30 20:27:35 +0530

    Update settings.py

--- webmail_backend/webmail_backend/settings.py
+++ webmail_backend/webmail_backend/settings.py
@@ -137,7 +137,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
-ADMIN_URL = 'http://admin.techcart.live'
 
 
 
